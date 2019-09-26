d56f9e9a6b8dbc0d186722bff2f1cc04118ea6b8
Roghashin Timbiti <rtimbiti@gmail.com>
2019-09-26 19:03:12 +0300

    connect to atlas db

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
+const graphqlSchema = require('./graphql/schema');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.use(
   '/eb_graphql',
   graphqlHttp({
-    schema: null,
-    rootValue: {},
+    schema: graphqlSchema,
+    rootValue: {
+      events: () => {},
+      createEvent: args => {
+        return {
+          ...args.name
+        };
+      }
+    },
     graphiql: true
   })
 );
 
-app.listen(3000);
+const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0-1rscx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    throw err;
+  });
